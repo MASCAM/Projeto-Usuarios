a5ed3ddff4ec5dd6491fd318591a4438fb175a1c
joaovitor <jvarantescabraltrab@gmail.com>
2020-05-11 00:55:36 -0300

    substitui o callback em getPhoto() por um Promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,43 +13,76 @@ class UserController {
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
+            let btn = this.formEl.querySelector("[type=submit]");
+            btn.disabled = true; //impede que o botão seja pressionado varias vezes
             let values = this.getValues();
-            this.getPhoto((content) => {
-                //passa essa função para ser executada no método
-                values.photo = content;
-                this.addLine(values);
-                //quando o FileReader chama o callback ele passa o argumento para cá
-                //no caso o arquivo lido, que então é armazenado em values.photo
-            });
-            
+            this.getPhoto().then(
+                //se resolve realiza a primeira função
+                //se reject realiza a segunda no promise da getPhoto()
+                (content) => {
+
+                    values.photo = content;
+                    this.addLine(values);
+                    this.formEl.reset(); 
+                    btn.disabled = false; //reseta o formulário e o botão submit
+
+                }, 
+
+                (e) => {
+
+                    console.error(e);
+
+                }
+
+            ); 
         
         });
 
     } //fechando onSubmit()
 
-    getPhoto(callback) { //pois o JS não sabe o real caminho do arquivo
+    getPhoto() { //pois o JS não sabe o real caminho do arquivo
         //**Nota: Foi muito difícil entender tudo isso**/
         //recebe uma função para o retorno da API fileReader
-        let fileReader = new FileReader();
-        let elements = [...this.formEl.elements].filter(item => { //filtra nos elementos onde está o elemento
-            //que se chama photo, se houver correspondência, retorna o item
-            //para posteriormente ser manipulado
-            if (item.name === 'photo') {
-                //a função filter filtra um array armazenando os itens procurados
-                //em um novo array
-                return item;
+        return new Promise((resolve, reject) => {
+            //Promise é uma classe por isso precisa ser cirada
+            //basicamente recebe uma função com dois argumentos que são duas funções
+            //caso resolve realiza a primeira função
+            //caso reject faz a segunda
+            let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter(item => { //filtra nos elementos onde está o elemento
+                //que se chama photo, se houver correspondência, retorna o item
+                //para posteriormente ser manipulado
+                if (item.name === 'photo') {
+                    //a função filter filtra um array armazenando os itens procurados
+                    //em um novo array
+                    return item;
+
+                }
+            
+            });
+            let file = (elements[0].files[0]);
+            fileReader.onload = () => {
+                //após carregar o arquivo, realiza uma função
+                resolve(fileReader.result);
+
+            };
+            fileReader.onerror = (e) => {
+
+                reject(e);
 
             }
-            
+            if (file) {
+                
+                fileReader.readAsDataURL(file); //permite a exibição da imagem na tela de cadastros
+
+            } else {
+
+                resolve('dist/img/boxed-bg.jpg');
+
+            }
+
         });
-        let file = (elements[0].files[0]);
-        fileReader.onload = () => {
-            //após carregar o arquivo, realiza uma função
-            callback(fileReader.result);
-
-        };
-        fileReader.readAsDataURL(file); //permite a exibição da imagem na tela de cadastros
-
+    
     } //fechando o getPhoto()
 
     getValues() {
@@ -62,8 +95,13 @@ class UserController {
                 if (field.checked) {
         
                     user[field.name] = field.value;
+                    
                 }
         
+            } else if (field.name == 'admin') {
+                
+                user[field.name] = field.checked;
+                
             } else {
         
                 user[field.name] = field.value;
@@ -87,19 +125,21 @@ class UserController {
 
     addLine(dataUser, tableId) {
 
-        this.tableEl.innerHTML = `
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
             <tr>
                         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
                         <td>${dataUser.email}</td>
-                        <td>${dataUser.admin}</td>
-                        <td>${dataUser.birth}</td>
+                        <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+                        <td>${dataUser.register}</td>
                         <td>
                           <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                         </td>
                       </tr>
         `;
+        this.tableEl.appendChild(tr);
     
     } //fechando o addLine();
 
